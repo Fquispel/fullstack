ad7c3c1144bdc125a7b1a9187079e167cface22e
Fernando Quispe Lefonzo <ferquispe.l@gmail.com>
2022-09-07 15:42:45 -0400

    Agregando Archivos con Mongoose

---
+++ server.js
@@ -0,0 +1,10 @@
+const  mongoose = require("mongoose");
+const Product = require("./models/Product");
+const app = require("./app");
+const port = process.env.PORT;
+mongoose.connect(process.env.DATABASE,{}).then((con) => {
+  console.log("Connected To Mongo");
+});
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
