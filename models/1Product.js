deb43bb6f52aef25309b7ef554d9449efcb323a4
Fernando Quispe Lefonzo <ferquispe.l@gmail.com>
2022-09-07 20:50:18 -0400

    Subiendo Archivos nuevos

---
+++ models/1Product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  productName: {
+    type:String,
+    required: true,
+    unique:true
+  },
+  price: {
+    type:Number,
+    required: [true, "Price is required"],
+  },
+  description: {
+    type:String,
+    required: false,
+  },
+});
+
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
