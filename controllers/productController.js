fd9fd0d7ec3605cda314a82433cff8540171f563
Fernando Quispe Lefonzo <ferquispe.l@gmail.com>
2022-09-08 20:26:27 -0400

    Actualizando Archivos Sequelize

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,9 @@
-//Handlers
-const fs = require('fs');
-const Product = require('../models/Product');
+const fs = require("fs");
+const { Product } = require('../models');
 
 exports.getAllProducts = async (req, res) => {
 
-  const products = await Product.find();
+  const products = await Product.findAll();
   res.status(200).json({
     status: "success",
     timeOfRequest: req.requestTime,
@@ -14,18 +13,19 @@ exports.getAllProducts = async (req, res) => {
     },
   });
 };
-exports.addProduct = async (req, res) => {
-  const newProduct = await Product.create(req.body);
+exports.addProduct =  async (req, res) => {
+  let newProduct =  Product.build(req.body);
+  newProduct = await newProduct.save();
   res.status(200).json({
     status: "success",
     data: {
-      product: newProduct,
+      product:newProduct,
     },
   });
 };
 
-exports.getProductById = async (req, res) => {
-  const foundProduct = await Product.findById(req.params.id);
+exports.getProductById =  async (req, res) => {
+  const foundProduct = await Product.findByPk(req.params.id);
   if (foundProduct) {
     res.status(200).json({
       status: "success",
